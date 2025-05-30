8d6be285d292cdf7b2bb248734822a8d2b67b0f0
Sarabjot Singh <sarabjotsingh@Sarabjots-MacBook-Pro.local>
2025-05-30 18:01:21 +0530

    implemented-add-fn-for-single-no-in-str+added-test-case-to-add-2-nos-in-str

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const add = (str) => {
     if (!str.length) return 0;
+    else return parseInt(str);
 }
 
 module.exports = add;
--- src/index.test.js
+++ src/index.test.js
@@ -4,6 +4,10 @@ test('should return 0 if empty string is passed to add fn', () => {
     expect(add("")).toBe(0);
 });
 
-test('', () => {
+test('should return same number if string contains 1 char', () => {
     expect(add("1")).toBe(1);
-})
+});
+
+test('should return addition of 2 nos if string contains 2 char seperated by ,', () => {
+    expect(add("1,2")).toBe(3);
+});
