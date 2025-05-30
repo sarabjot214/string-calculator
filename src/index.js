64d91d27184d02f4ecc6af23a4b9c272f2ea0ab7
Sarabjot Singh <sarabjotsingh@Sarabjots-MacBook-Pro.local>
2025-05-30 18:59:01 +0530

    impl-fn-to-add-multipleNos-in-str-sepr-by-diff-delimeter+add-test-to-throw-error-on-neg-nos

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const add = (str) => {
-    const delimeter = /,|\n/;
-    const nums = str.split(delimeter).map(Number);
+    let delimeter = /,|\n/,
+        numStr = str;
+    
+        if (str.includes("//")) {
+        let temp = str.split('\n');
+        delimeter = temp[0].slice(2);
+        numStr = temp[1];
+    }
+
+    const nums = numStr.split(delimeter).map(Number);
     const sum = nums.reduce(((sum, num) => sum + num), 0);
     return sum;
-}
+};
 
 module.exports = add;
--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,4 @@
-let add = require ('./index');
+let add = require('./index');
 
 test('should return 0 if empty string is passed to add fn', () => {
     expect(add("")).toBe(0);
@@ -26,4 +26,8 @@ test('should return sum w.r.t addition of multiple nos if string contains multip
 
 test('should return sum w.r.t addition of multiple nos if string contains multiple chars seperated by diff delimeter', () => {
     expect(add("//;\n1;2")).toBe(3);
+});
+
+test('should throw an error for negative numbers', () => {
+    expect(add('1,-2')).toThrow('negative numbers not allowed -2');
 });
